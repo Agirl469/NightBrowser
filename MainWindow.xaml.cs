35187e918f8d73522a5003bceafa62105fdd4f79
Kornixdude <127904660+Agirl469@users.noreply.github.com>
2026-07-05 10:05:56 -0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Web.WebView2.Core;
+
+namespace NightBrowser.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Browser.EnsureCoreWebView2Async();
+            Browser.CoreWebView2.Navigate("https://example.com");
+        }
+    }
+}
